b12267483545bfcd972c90b0213894c48f6a0348
Manideep Pilli <manideep.pmd@gmail.com>
2025-10-31 13:18:46 -0600

    ADDED: Print statement

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Program
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a sample C# program.");
+            Console.Beep();
+        }
+    }
+}
